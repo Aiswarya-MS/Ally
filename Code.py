7b7a9385c3c5febff340cc8050afe494bac42557
Anitha k <anithabalasubramanian75@gmail.com>
2021-09-26 21:51:24 +0530

    written the code for notifier

---
+++ Code.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+
+title=' Hey Ally Here'
+message='Hey your eyes are stressed out ,take rest , Let your beautiful eyes bright more'
+
+notification.notify(title = title,message = message, timeout =10, toast = False)
+
+#time.sleep(10)
