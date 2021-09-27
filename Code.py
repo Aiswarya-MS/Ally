4081aa81a40b03f4318f0bb87a0e8206e57527ed
Aiswarya-MS <aiswaryamsaisw@gmail.com>
2021-09-27 21:21:53 +0530

    added time delay

--- Code.py
+++ Code.py
@@ -3,7 +3,6 @@ from plyer import notification
 
 title=' Hey Ally Here'
 message='Hey your eyes are stressed out ,take rest , Let your beautiful eyes bright more'
-
-notification.notify(title = title,message = message, timeout =10, toast = False)
-
-#time.sleep(10)
+while True:
+    notification.notify(title = title,message = message, timeout =10, toast = False)
+    time.sleep(1200)
